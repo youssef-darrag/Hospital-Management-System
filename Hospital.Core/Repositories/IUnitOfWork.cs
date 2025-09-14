24cae95cdc0c6710ee6eb4a5403b6bea2b94b56c
Youssef-Darrag <darrag904@gmail.com>
2025-09-15 00:33:04 +0300

    Add unit of work class.

---
+++ Hospital.Core/Repositories/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using Hospital.Core.Models;
+
+namespace Hospital.Core.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<ApplicationUser> ApplicationUsers { get; }
+
+        int Complete();
+        Task<int> CompleteAsync();
+    }
+}
---
+++ Hospital.EF/Repositories/UnitOfWork.cs
@@ -0,0 +1,49 @@
+﻿using Hospital.Core.Models;
+using Hospital.Core.Repositories;
+
+namespace Hospital.EF.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IGenericRepository<ApplicationUser> ApplicationUsers { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+
+            ApplicationUsers = new GenericRepository<ApplicationUser>(_context);
+        }
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public async Task<int> CompleteAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        private bool disposed = false;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+    }
+}
