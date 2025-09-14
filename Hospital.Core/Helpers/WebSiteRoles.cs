9895d12478672c7e627b44e1cf648516caeb7d51
Youssef-Darrag <darrag904@gmail.com>
2025-09-14 22:37:09 +0300

    Add website roles.

---
+++ Hospital.Core/Helpers/WebSiteRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace Hospital.Core.Helpers
+{
+    public static class WebSiteRoles
+    {
+        public const string Admin = "Admin";
+        public const string Doctor = "Doctor";
+        public const string Patient = "Patient";
+    }
+}
