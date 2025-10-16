7329cf57fa81518e6473f2c40c7c3f1bc33323d3
Youssef-Darrag <darrag904@gmail.com>
2025-10-17 00:14:18 +0300

    Add notification model.

---
+++ Hospital.Core/Models/Notification.cs
@@ -0,0 +1,12 @@
+﻿namespace Hospital.Core.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; } = default!;
+        public string Message { get; set; } = default!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public bool IsRead { get; set; }
+        public DateTime? ReadAt { get; set; }
+    }
+}
