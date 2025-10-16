1461ee7aa4aaf7b76d4f0c7d5d8576abd153001d
Youssef-Darrag <darrag904@gmail.com>
2025-10-17 00:23:30 +0300

    Add notification view model.

---
+++ Hospital.Core/ViewModels/NotificationsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Hospital.Core.Models;
+
+namespace Hospital.Core.ViewModels
+{
+    public class NotificationsViewModel
+    {
+        public IEnumerable<Notification> Notifications { get; set; } = new List<Notification>();
+        public int UnreadCount { get; set; }
+        public int TotalCount { get; set; }
+        public bool HasUnread => UnreadCount > 0;
+        public bool HasNotifications => Notifications.Any();
+    }
+}
