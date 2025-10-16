58d12fb50b43d37b45dce342f145e626b4eb91df
Youssef-Darrag <darrag904@gmail.com>
2025-10-17 00:21:26 +0300

    Update notification table.

---
+++ Hospital.EF/Migrations/20251003012828_AddReadAtColumnToNotificationsTable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hospital.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReadAtColumnToNotificationsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReadAt",
+                table: "Notifications",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReadAt",
+                table: "Notifications");
+        }
+    }
+}
