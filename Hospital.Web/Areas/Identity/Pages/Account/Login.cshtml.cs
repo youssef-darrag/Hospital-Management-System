bf24986cdeeca4f89790968b12116fbe8ae0bcc8
Youssef-Darrag <darrag904@gmail.com>
2025-10-20 13:22:31 +0300

    Modify the login process.

--- Hospital.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Hospital.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -2,18 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
 
 namespace Hospital.Web.Areas.Identity.Pages.Account
 {
@@ -107,11 +100,15 @@ namespace Hospital.Web.Areas.Identity.Pages.Account
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
+                return Page();
+
+            var user = await _signInManager.UserManager.FindByEmailAsync(Input.Email);
+            if (user != null)
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(user, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
@@ -126,14 +123,12 @@ namespace Hospital.Web.Areas.Identity.Pages.Account
                     _logger.LogWarning("User account locked out.");
                     return RedirectToPage("./Lockout");
                 }
-                else
-                {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-                    return Page();
-                }
             }
 
-            // If we got this far, something failed, redisplay form
+            ModelState.Remove("Input.Email");
+            Input.Email = string.Empty;
+
+            ModelState.AddModelError(string.Empty, "Email or Password is incorrect!");
             return Page();
         }
     }
