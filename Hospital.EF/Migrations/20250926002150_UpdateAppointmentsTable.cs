d4a0f8b4129d1945aef56834754495c569fb9de8
Youssef-Darrag <darrag904@gmail.com>
2025-10-21 07:51:04 +0300

    Add migration: Update appointments table.

---
+++ Hospital.EF/Migrations/20250926002150_UpdateAppointmentsTable.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hospital.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAppointmentsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "Number",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Appointments");
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "AppointmentDate",
+                table: "Appointments",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "EndTime",
+                table: "Appointments",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeOnly(0, 0, 0));
+
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "StartTime",
+                table: "Appointments",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeOnly(0, 0, 0));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Appointments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TimingId",
+                table: "Appointments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_TimingId",
+                table: "Appointments",
+                column: "TimingId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointments_Timings_TimingId",
+                table: "Appointments",
+                column: "TimingId",
+                principalTable: "Timings",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointments_Timings_TimingId",
+                table: "Appointments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_TimingId",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "AppointmentDate",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "EndTime",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "StartTime",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "TimingId",
+                table: "Appointments");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Appointments",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Number",
+                table: "Appointments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Appointments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
