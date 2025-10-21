c78c3029f78f7321035851175f12214b8e5fc3e3
Youssef-Darrag <darrag904@gmail.com>
2025-10-21 07:49:14 +0300

    Modify appointment class.

--- Hospital.Core/Models/Appointment.cs
+++ Hospital.Core/Models/Appointment.cs
@@ -3,13 +3,16 @@
     public class Appointment
     {
         public int Id { get; set; }
-        public string Number { get; set; } = default!;
-        public string Type { get; set; } = default!;
-        public DateTime CreatedDate { get; set; }
+        public DateOnly AppointmentDate { get; set; }
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly EndTime { get; set; }
+        public Status Status { get; set; }
         public string Description { get; set; } = default!;
         public string DoctorId { get; set; } = default!;
         public ApplicationUser Doctor { get; set; } = default!;
         public string PatientId { get; set; } = default!;
         public ApplicationUser Patient { get; set; } = default!;
+        public int TimingId { get; set; }
+        public Timing Timing { get; set; } = default!;
     }
 }
