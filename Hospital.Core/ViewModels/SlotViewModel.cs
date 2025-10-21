781875dcde934d96e3adf11e2177952e1a4785cd
Youssef-Darrag <darrag904@gmail.com>
2025-10-21 07:53:47 +0300

    Add slot view model.

---
+++ Hospital.Core/ViewModels/SlotViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Hospital.Core.ViewModels
+{
+    public class SlotViewModel
+    {
+        public TimeOnly Start { get; set; }
+        public TimeOnly End { get; set; }
+        public bool IsBooked { get; set; }
+    }
+}
