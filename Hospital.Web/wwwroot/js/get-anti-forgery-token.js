d1d8de383b68412490f0b8372d75280d3e79eac0
Youssef-Darrag <darrag904@gmail.com>
2025-10-23 01:46:39 +0300

    Add get antiforgery token js file.

---
+++ Hospital.Web/wwwroot/js/get-anti-forgery-token.js
@@ -0,0 +1,4 @@
+﻿function getAntiForgeryToken() {
+    return document.querySelector('input[name="__RequestVerificationToken"]')?.value ||
+           document.querySelector('[name="__RequestVerificationToken"]')?.value;
+}
